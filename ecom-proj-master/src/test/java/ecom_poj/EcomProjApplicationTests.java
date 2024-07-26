046e5a45b4de31bd2e1a5fa4117a0fba3a73aeaa
vits17 <126216579+vits17@users.noreply.github.com>
2024-07-26 19:27:14 +0530

    backend

---
+++ ecom-proj-master/src/test/java/ecom_poj/EcomProjApplicationTests.java
@@ -0,0 +1,13 @@
+package ecom_poj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
